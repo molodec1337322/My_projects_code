ccad52f33269e0d59be517feff52e27bdd781e09
molodec1337322 <54796286+molodec1337322@users.noreply.github.com>
2019-09-07 22:54:25 +0300

    Update C#_snake(Unfinished).cs

--- C#_snake(Unfinished).cs
+++ C#_snake(Unfinished).cs
@@ -10,14 +10,14 @@ namespace Snake
     class SnakeSegment
     {
         public int itsPosX, itsPosY, itsSize;
-        public static int lenght = 0;
+        public static int length = 0;
 
         public SnakeSegment(int newPosX, int newPosY, int newSize)
         {
             itsPosX = newPosX;
             itsPosY = newPosY;
             itsSize = newSize;
-            lenght++;
+            length++;
         }
 
         public int GetX() {return itsPosX; }
@@ -33,31 +33,37 @@ namespace Snake
 
     class Snake
     {
-        private int itsLenght;
         private SnakeSegment[] itsSegments;
+        private int segSize;
 
-        public Snake(int lenght, SnakeSegment[] segments)
+        public Snake(SnakeSegment[] segments, int segSize)
         {
-            itsLenght = lenght;
             itsSegments = segments;
+            this.segSize = segSize;
         }
-
-        public int GetLenth() { return itsLenght; }
-        public void moveSegments(int newPosX, int newPosY)
+        public void MoveSegments(int newPosX, int newPosY)
         {
-            itsSegments[0].SetPos(newPosX, newPosY);
-            for (int i=itsLenght; i>0; i--)
+            for (int i=SnakeSegment.length-1; i>0; i--)
             {
                 itsSegments[i].SetPos(itsSegments[i - 1].GetX(), itsSegments[i - 1].GetY());
             }
+            itsSegments[0].SetPos(newPosX, newPosY);
+        }
+        public void AddSegment()
+        {
+            Array.Resize(ref itsSegments, SnakeSegment.length + 1);
+            itsSegments[SnakeSegment.length] = new SnakeSegment(-1, -1, segSize);
         }
         public void showSegs()
         {
             foreach(SnakeSegment segment in itsSegments)
             {
                 Console.Write(segment.GetX().ToString());
+                Console.Write(" ");
                 Console.Write(segment.GetY().ToString());
+                Console.Write("\n");
             }
+            Console.Write("\n");
         }
     }
 
@@ -68,13 +74,20 @@ namespace Snake
         {
             const int segSize = 20;
             SnakeSegment[] segments = new SnakeSegment[3]
-                { new SnakeSegment(0, 1, segSize),
+                { new SnakeSegment(0, 3, segSize),
                     new SnakeSegment(0, 2, segSize),
-                    new SnakeSegment(0, 3, segSize)};
+                    new SnakeSegment(0, 1, segSize)};
 
-            Snake snake = new Snake(SnakeSegment.lenght, segments);
+            Snake snake = new Snake(segments, segSize);
+            snake.showSegs();
+            snake.MoveSegments(0, 4);
+            snake.showSegs();
+            snake.AddSegment();
+            snake.showSegs();
+            snake.MoveSegments(0, 5);
             snake.showSegs();
             Console.ReadKey();
         }
+
     }
 }

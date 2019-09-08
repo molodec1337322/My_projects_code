d93b63b529c88a6415dc41d89265e5995e61e998
molodec1337322 <54796286+molodec1337322@users.noreply.github.com>
2019-09-08 14:53:57 +0300

    Update C#_snake(Unfinished).cs

--- C#_snake(Unfinished).cs
+++ C#_snake(Unfinished).cs
@@ -38,7 +38,7 @@ namespace Snake
         private SnakeSegment[] itsSegments;
         private string segChar;
 
-        public Snake(SnakeSegment[] segments, string segChar)
+        public Snake(ref SnakeSegment[] segments, string segChar)
         /* Конструктор */
         {
             itsSegments = segments;
@@ -55,40 +55,53 @@ namespace Snake
         public void AddSegment()
         {
             Array.Resize(ref itsSegments, SnakeSegment.length + 1);
-            itsSegments[SnakeSegment.length] = new SnakeSegment(-10, -10, segChar);
+            itsSegments[SnakeSegment.length - 1] = new SnakeSegment(-10, -10, segChar);
+        }
+
+        public bool CheckSnakeAlive()
+        {
+            if (itsSegments[0].GetX() == 0 || itsSegments[0].GetY() == 0 || itsSegments[0].GetX() == 14 || itsSegments[0].GetY() == 29)
+            {
+                return false;
+            }
+            for(int i=1; i<SnakeSegment.length; i++)
+            {
+                if (itsSegments[0].GetX() == itsSegments[i].GetX() && itsSegments[0].GetY() == itsSegments[i].GetY())
+                {
+                    return false;
+                }
+            }
+            return true;
         }
 
         public int GetFirstX() { return itsSegments[0].GetX(); }
         public int GetFirstY() { return itsSegments[0].GetY(); }
         public SnakeSegment GetSegment(int index) { return itsSegments[index]; }// возвращает элемент змейки с индексом index
-
-
-
-        public void showSegs()
-        {
-            foreach (SnakeSegment segment in itsSegments)
-            {
-                Console.Write(segment.GetX().ToString());
-                Console.Write(" ");
-                Console.Write(segment.GetY().ToString());
-                Console.Write("\n");
-            }
-            Console.Write("\n");
-        }
     }
 
     class Apple
     {
         private int itsPosX;
         private int itsPosY;
-        public Apple(int newPosX, int newPosY)
+        private string itsChar;
+        private Random rnd;
+        public Apple(int newPosX, int newPosY, string newChar, Random rnd)
         {
             itsPosX = newPosX;
             itsPosY = newPosY;
+            itsChar = newChar;
+            this.rnd = rnd;
+        }
+
+        public void Relocate()
+        {
+            itsPosX = rnd.Next()%13+1;
+            itsPosY = rnd.Next()%28+1;
         }
 
         public int GetX() { return itsPosX; }
         public int GetY() { return itsPosY; }
+        public string GetChar() { return itsChar; }
     }
 
     class Graphics
@@ -96,12 +109,14 @@ namespace Snake
         private int sizeX;
         private int sizeY;
         private Snake snake;
+        private Apple apple;
         private string[,] gameField;
-        public Graphics(int sizeX, int sizeY, Snake snake)
+        public Graphics(int sizeX, int sizeY, Snake snake, Apple apple)
         {
             this.sizeX = sizeX;
             this.sizeY = sizeY;
             this.snake = snake;
+            this.apple = apple;
             gameField = new string[sizeX, sizeY];
 
             //первичное заполнение поля
@@ -124,17 +139,12 @@ namespace Snake
 
         public void Update()
         {
-            for (int x = 1; x < sizeX-1; x++)
-            {
-                for (int y = 1; y < sizeY-1; y++)
-                {
-                    gameField[x, y] = " ";
-                }
-            }
             for (int i = 0; i < SnakeSegment.length; i++)
             {
                 gameField[snake.GetSegment(i).GetX(), snake.GetSegment(i).GetY()] = snake.GetSegment(i).GetChar();
             }
+            gameField[snake.GetSegment(SnakeSegment.length - 1).GetX(), snake.GetSegment(SnakeSegment.length - 1).GetY()] = " ";
+            gameField[apple.GetX(), apple.GetY()] = apple.GetChar();
         }
 
         public void Draw()
@@ -158,42 +168,65 @@ namespace Snake
 
         static void Main(string[] args)
         {
-            string segChar = "%";
-            SnakeSegment[] segments = new SnakeSegment[3]
+            string segChar = "0";
+            SnakeSegment[] segments = new SnakeSegment[4]
                 { new SnakeSegment(3, 7, "#"),
                     new SnakeSegment(3, 6, segChar),
-                    new SnakeSegment(3, 5, segChar)}; // тоже по факту костыль, но ладно
+                    new SnakeSegment(3, 5, segChar),
+                    new SnakeSegment(3, 4, segChar)}; // тоже по факту костыль, но ладно
 
-            Snake snake = new Snake(segments, segChar);
-            snake.showSegs();
+            Random rnd = new Random();
 
-            Apple apple = new Apple(10, 10);
+            Snake snake = new Snake(ref segments, segChar);
 
-            Graphics g = new Graphics(15, 30, snake);
+            Apple apple = new Apple(10, 10, "A", rnd);
+
+
+            Graphics g = new Graphics(15, 30, snake, apple);
             g.Draw();
 
             ConsoleKeyInfo dir;
-            while (true)
+            while (snake.CheckSnakeAlive())
             {
                 dir = Console.ReadKey();
-                if(dir.Key == ConsoleKey.D2)
+                if(dir.Key == ConsoleKey.DownArrow)
                 {
                     snake.MoveSegments(snake.GetFirstX() + 1, snake.GetFirstY());
+                    if(snake.GetFirstX() == apple.GetX() && snake.GetFirstY() == apple.GetY())
+                    {
+                        snake.AddSegment();
+                        apple.Relocate();
+                    }
                     g.Draw();
                 }
-                else if(dir.Key == ConsoleKey.D8)
+                else if(dir.Key == ConsoleKey.UpArrow)
                 {
                     snake.MoveSegments(snake.GetFirstX() - 1, snake.GetFirstY());
+                    if (snake.GetFirstX() == apple.GetX() && snake.GetFirstY() == apple.GetY())
+                    {
+                        snake.AddSegment();
+                        apple.Relocate();
+                    }
                     g.Draw();
                 }
-                else if(dir.Key == ConsoleKey.D6)
+                else if(dir.Key == ConsoleKey.RightArrow)
                 {
                     snake.MoveSegments(snake.GetFirstX(), snake.GetFirstY() + 1);
+                    if (snake.GetFirstX() == apple.GetX() && snake.GetFirstY() == apple.GetY())
+                    {
+                        snake.AddSegment();
+                        apple.Relocate();
+                    }
                     g.Draw();
                 }
-                else if(dir.Key == ConsoleKey.D4)
+                else if(dir.Key == ConsoleKey.LeftArrow)
                 {
                     snake.MoveSegments(snake.GetFirstX(), snake.GetFirstY() - 1);
+                    if (snake.GetFirstX() == apple.GetX() && snake.GetFirstY() == apple.GetY())
+                    {
+                        snake.AddSegment();
+                        apple.Relocate();
+                    }
                     g.Draw();
                 }
                 else

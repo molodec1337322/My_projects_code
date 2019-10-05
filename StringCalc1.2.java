12b973b65887067e74a9643be1694668fa040df6
molodec1337322 <54796286+molodec1337322@users.noreply.github.com>
2019-10-05 16:31:37 +0300

    Create StringCalc1.2.java

---
+++ StringCalc1.2.java
@@ -0,0 +1,186 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    private static ParseEngine parser = new ParseEngine();
+
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+        String example = userInput.nextLine();
+
+        parser.parseString(example);
+        System.out.println(parser.getResult());
+    }
+}
+
+class ParseEngine{
+
+    enum Operands{Plus, Minus, Multiply, Division};
+
+    private ArrayList<String> numbers = new ArrayList<>();
+    private ArrayList<Operands> operands = new ArrayList<>();
+    private ArrayList<Integer> priority = new ArrayList<>();
+
+    private void addOperand(char operand){
+        switch (operand){
+            case '+':
+                operands.add(Operands.Plus);
+                break;
+            case '-':
+                operands.add(Operands.Minus);
+                break;
+            case '*':
+                operands.add(Operands.Multiply);
+                break;
+            case '/':
+                operands.add(Operands.Division);
+                break;
+        }
+    }
+
+    private boolean isOperand(char symbol){
+        return (symbol == '+' || symbol == '-' || symbol == '*' || symbol == '/');
+    }
+
+    private void resetExample(){
+        numbers.clear();
+        operands.clear();
+        priority.clear();
+    }
+
+    public void parseString(String example){
+        resetExample();
+        StringBuilder temp = new StringBuilder();
+
+        int priority = 0;
+        int openBrackets = 0;
+        int closeBrackets = 0;
+
+        for(int i = 0; i < example.length(); i++){
+
+            if (example.charAt(i) == '('){
+                openBrackets++;
+                priority++;
+            }
+            else if(example.charAt(i) == ')'){
+                closeBrackets++;
+                priority--;
+            }
+
+            if (!isOperand(example.charAt(i)) && example.charAt(i) != '(' && example.charAt(i) != ')'){
+                temp.append(example.charAt(i));
+            }
+            else if(isOperand(example.charAt(i))){
+                numbers.add(String.valueOf(temp));
+                temp.setLength(0);
+                addOperand(example.charAt(i));
+                this.priority.add(priority);
+            }
+
+            if(i == example.length() - 1 && temp.length() > 0){
+                numbers.add(String.valueOf(temp));
+                temp.setLength(0);
+            }
+        }
+
+        if(checkCorrectness(openBrackets, closeBrackets)) eval();
+    }
+
+    //возвращает индекс операнда с наибольшим приоритетом
+    private int getHighestPriorityIndex(boolean isHighPriority){
+        int priorityIndex = getHighestPriority();
+        if(isHighPriority){
+            for(int i = 0; i < priority.size(); i++){
+                if(priority.get(i) == priorityIndex &&
+                        (operands.get(i) == Operands.Division || operands.get(i) == Operands.Multiply)){
+                    return i;
+                }
+            }
+            return -1;
+        }
+        else{
+            for(int i = 0; i < priority.size(); i++){
+                if(priority.get(i) == priorityIndex &&
+                        (operands.get(i) == Operands.Minus || operands.get(i) == Operands.Plus)){
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+
+    //возвращает наибольший приоритет
+    private int getHighestPriority(){
+        int highestPriority = 0;
+        for(Integer priority: this.priority){
+            if(priority > highestPriority) highestPriority = priority;
+        }
+        return highestPriority;
+    }
+
+    private void editNumbersOperands(int index, double result){
+        numbers.set(index, String.valueOf(result));
+        numbers.remove(index + 1);
+        operands.remove(index);
+        priority.remove(index);
+    }
+
+    private boolean checkCorrectness(int countOpenBrackets, int countCloseBrackets){
+        if (numbers.size() == operands.size() ||
+                countOpenBrackets - countCloseBrackets != 0){
+            resetExample();
+            numbers.add("");
+            return false;
+        }
+        return true;
+    }
+
+    private void eval(){
+        while(operands.size() > 0){
+
+            int i = getHighestPriorityIndex(true);
+            double tempResult;
+            while(getHighestPriorityIndex(true) != -1){
+                if(operands.get(i) == Operands.Multiply){
+                    tempResult = Double.parseDouble(numbers.get(i)) * Double.parseDouble(numbers.get(i+1));
+                    editNumbersOperands(i, tempResult);
+                }
+                else if(operands.get(i) == Operands.Division){
+                    tempResult = Double.parseDouble(numbers.get(i)) / Double.parseDouble(numbers.get(i+1));
+                    editNumbersOperands(i, tempResult);
+                }
+                else{
+                    i = getHighestPriorityIndex(true);
+                    if(i == -1){
+                        break;
+                    }
+                }
+            }
+
+            i = getHighestPriorityIndex(false);
+            while(getHighestPriorityIndex(false) != -1){
+                if(operands.get(i) == Operands.Plus){
+                    tempResult = Double.parseDouble(numbers.get(i)) + Double.parseDouble(numbers.get(i+1));
+                    editNumbersOperands(i, tempResult);
+                }
+                else if(operands.get(i) == Operands.Minus){
+                    tempResult = Double.parseDouble(numbers.get(i)) - Double.parseDouble(numbers.get(i+1));
+                    editNumbersOperands(i, tempResult);
+                }
+                else{
+                    i = getHighestPriorityIndex(false);
+                    if(i == -1){
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    public String getResult(){
+        return numbers.get(0);
+    }
+}

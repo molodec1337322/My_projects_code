f76107e89521965cf0c62e126b300dca61845968
molodec1337322 <54796286+molodec1337322@users.noreply.github.com>
2019-10-25 15:57:28 +0300

    Update and rename C#MathParser(Unfinished) to C#ChangableArray.cs

---
+++ C#ChangableArray.cs
@@ -0,0 +1,56 @@
+using System;
+
+namespace StringCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class DynamicArray<T>
+    {
+        private T[] Arr;
+        private int Size;
+        DynamicArray(int Size)
+        {
+            this.Size = Size;
+            this.Arr = new T[this.Size];
+        }
+
+        DynamicArray(int Size, params T[] ArrData)
+        {
+            this.Size = Size;
+            this.Arr = new T[this.Size];
+            try
+            {
+                ArrData.CopyTo(Arr, 0);
+            }
+            catch(Exception ex)
+            {
+                for (int i = 0; i < Size; i++) Arr[i] = default(T);
+            }
+        }
+
+        public void Push(T element)
+        {
+            Array.Resize(ref Arr, Size);
+            Arr[Size] = element;
+            Size++;
+        }
+
+        public T Get(int pos)
+        {
+            try
+            {
+                return Arr[pos];
+            }
+            catch(IndexOutOfRangeException ex)
+            {
+                return default(T);
+            }
+        }
+    }
+}
+

fca1f2208357d7b4e7b64a2a463fefdbe5b238e3
molodec1337322 <54796286+molodec1337322@users.noreply.github.com>
2020-05-03 01:03:49 +0300

    Create linear regression corona(need to fix).py

---
+++ linear regression corona(need to fix).py
@@ -0,0 +1,19 @@
+import numpy as np
+from numpy import linalg
+#from urllib.request import urlopen
+
+confirmed = np.array([[1, 3472653], [1, 2994885], [1, 2555266], [1, 2162516], [1, 1779786]])
+deaths = np.array([[243998], [206917], [177412], [145430], [108740]])
+
+X = confirmed
+Y = deaths
+
+b = linalg.inv(X.T.dot(X)).dot(X.T).dot(Y)
+epsilon = Y - X.dot(b)
+err = epsilon.T.dot(epsilon)
+print(err)
+
+print("Введите количество: ")
+user_confirmed = int(input())
+
+print(b[0] + user_confirmed * b[1])

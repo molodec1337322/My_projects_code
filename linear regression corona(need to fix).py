f49752b1245ef10b1d131bcf7cca8f22b11e01d6
molodec1337322 <54796286+molodec1337322@users.noreply.github.com>
2020-05-03 11:50:04 +0300

    Update linear regression corona(need to fix).py

--- linear regression corona(need to fix).py
+++ linear regression corona(need to fix).py
@@ -1,9 +1,10 @@
 import numpy as np
 from numpy import linalg
+import matplotlib.pyplot as plt
 #from urllib.request import urlopen
 
-confirmed = np.array([[1, 3472653], [1, 2994885], [1, 2555266], [1, 2162516], [1, 1779786]])
-deaths = np.array([[243998], [206917], [177412], [145430], [108740]])
+confirmed = np.array([[1, 3472653], [1, 2994885], [1, 2555266], [1, 2162516], [1, 1779786], [1, 0]])
+deaths = np.array([[243998], [206917], [177412], [145430], [108740], [0]])
 
 X = confirmed
 Y = deaths
@@ -12,7 +13,16 @@ b = linalg.inv(X.T.dot(X)).dot(X.T).dot(Y)
 epsilon = Y - X.dot(b)
 err = epsilon.T.dot(epsilon)
 
-print("Введите количество: ")
-user_confirmed = int(input())
 
-print(b[0] + user_confirmed * b[1])
+x = np.arange(5000000)
+y = b[0] + b[1] * x
+
+fig = plt.figure()
+for i in range(deaths.size):
+    plt.scatter(confirmed[i][1], deaths[i])
+
+plt.plot(x, y, label='line', color='red')
+
+plt.show()
+
+print(b, err)
